8ebe7635ac42ecbffdfb9d53691e6e970932e216
Kaszper2001 <62875250+Kaszper2001@users.noreply.github.com>
2020-05-21 18:01:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("test")
+const button = document.querySelector(".button");
+const random = document.querySelector(".random");
+var losowa = ['POSTACIE', 'KRÓLESTWA', 'KAMIENIE', 'BOSSY'];
+
+button.addEventListener('click', function(){
+
+    random.value= losowa [Math.floor(Math.random() * losowa.length)];
+
+
+
+
+
+
+    if (random.value == "POSTACIE"){
+        window.location.replace("postacie.html");   
+    }
+
+    if (random.value == "KRÓLESTWA" ){
+        window.location.replace("królestwa.html");   
+    }
+
+    if (random.value == "KAMIENIE" ){
+        window.location.replace("kamienie.html");   
+    }
+
+    if (random.value == "BOSSY" ){
+        window.location.replace("bossy.html"); 
+    }
+})
